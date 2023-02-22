3c8fb27827ec95228aa5d9f30dac3ee7bce0335f
Paul Maier <pauljustus279@gmail.com>
2023-02-22 20:01:59 +0100

    Really last changes

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-MAX_EXECUTION_TIME = 60 * 60 * 3  # 3 hours
+MAX_EXECUTION_TIME = 60 * 60 * 2.5  # 3 hours
 MAX_SIZE_DATA: int = 2 * 1000 * 1000 * 1000  # 3 GB
 
 ORIGINAL_IMAGE_NAME = "original.png"
